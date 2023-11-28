01e5540b77ff978bfdfd070f804b01645bab9dfd
Dmitry Redkin <tanatonaut@gmail.com>
2023-11-28 17:35:31 +0200

    assertEq

--- test/StdCheats.t.sol
+++ test/StdCheats.t.sol
@@ -229,8 +229,8 @@ contract StdCheatsTest is Test {
         string memory mnemonic = "test test test test test test test test test test test junk";
 
         (address deployer, uint256 privateKey) = deriveRememberKey(mnemonic, 0);
-        assertEq(deployer, 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
-        assertEq(privateKey, 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
+        assertEq(deployer, address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266));
+        assertEq(privateKey, "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80");
     }
 
     function test_BytesToUint() public {
