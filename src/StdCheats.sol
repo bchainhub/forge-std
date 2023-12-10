c842aea92297bb0e665e3c77955fb202e617e6f5
Dmitry Redkin <tanatonaut@gmail.com>
2023-12-10 14:02:23 +0200

    test

--- src/StdCheats.sol
+++ src/StdCheats.sol
@@ -515,8 +515,8 @@ abstract contract StdCheatsSafe {
 
     // creates a labeled address and the corresponding private key
     function makeAddrAndKey(string memory name) internal virtual returns (address addr, string memory privateKey) {
-        // privateKey = uint256(keccak256(abi.encodePacked(name)));
-        privateKey = "1123";
+        privateKey = uint256(keccak256(abi.encodePacked(name)));
+        // privateKey = "1123";
         addr = vm.addr(privateKey);
         vm.label(addr, name);
     }
