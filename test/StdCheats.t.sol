719817db3019e310f695083b1c5463f603f34ffe
Mykhailo Slyvka <misha.slyvka@wallit.eu>
2024-03-28 00:31:36 +0200

    return mainnet create2address in mainnet

--- test/StdCheats.t.sol
+++ test/StdCheats.t.sol
@@ -397,7 +397,7 @@ contract StdCheatsTest is Test {
         stdCheatsMock.exposed_assumeNotPayable(Checksum.toIcan(uint160(bytes20(hex"000000000000000000636f6e736f6c652e6c6f67")))); 
 
         // Create2Deployer
-        stdCheatsMock.exposed_assumeNotPayable(Checksum.toIcan(uint160(bytes20(hex"ab800ee5e10bfbd37bc647e01d94489b4e244817b07f")))); 
+        stdCheatsMock.exposed_assumeNotPayable(Checksum.toIcan(uint160(bytes20(hex"3edadf999cb7b8b3ebc71f5e97783176d289d640")))); 
 
         // all should revert since these addresses are payable
 
